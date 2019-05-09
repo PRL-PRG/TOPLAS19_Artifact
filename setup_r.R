a9d7575875135aca38367b3b6ebf99931194a755
peta <peta.maj82@gmail.com>
2019-05-09 08:46:18 +0200

    R setup in separate file.

---
+++ setup_r.R
@@ -0,0 +1,22 @@
+# Install the required R packages
+
+install.packages(c(
+    "rmarkdown"
+    "car",
+    "data.table",
+    "assertthat",
+    "fst",
+    "stringr",
+    "plyr",
+    "ggplot2",
+    "ggrepel",
+    "scales",
+    "xtable",
+    "MASS",
+    "gplots",
+    "dplyr",
+    "gridExtra",
+    "grid",
+    "tidyr",
+    dependencies = T
+))
